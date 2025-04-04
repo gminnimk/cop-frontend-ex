bf8754c72e32e5496e390db352db91c8802d84da
gminnimk <tvt1245@gmail.com>
2025-04-04 21:53:43 +0900

    feat: default.vue

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,14 @@
-<template>
-  <v-main>
-    <router-view />
-  </v-main>
-</template>
-
 <script setup>
-  //
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
 </script>
+
+<template>
+  <v-app>
+    <AppHeader/>
+    <v-main>
+      <router-view/>
+    </v-main>
+    <AppFooter/>
+  </v-app>
+</template>
